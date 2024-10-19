2d48e50f542a11cc42c00c2002852f39c7045352
FAROUKDEV7 <faroukeldreinydev74@gmail.com>
2024-10-19 03:16:54 +0300

    add update product

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('add_cart',views.add_cart,name="add_cart"),
-    path('update_cart',views.update_cart,name="update_cart"),
-    path('delete_cart',views.delete_cart,name="delete_cart"),
+    path('cart_summary/',views.cart_summary,name='cart_summary'),
+    path('add_cart/',views.add_cart,name="add_cart"),
+    path('update_cart/',views.update_cart,name="update_cart"),
+    path('delete_cart/',views.delete_cart,name="delete_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -4,12 +4,28 @@ from pages.models import Product
 from django.http import JsonResponse
 # Create your views here.
 
+def cart_summary(request):
+    cart=Cart(request)
+    product=cart.get_prods
+    quantities=cart.get_quants
+    context={
+        'product':product,
+        'quantities':quantities,
+    }
+    return render(request,'cart/cart_summary.html',context)
+
+
+
+
+
+
 def add_cart(request):
     cart=Cart(request)
     if request.POST.get("action") == "post":
         product_id=int(request.POST.get('product_id'))
+        product_qty=int(request.POST.get('product_qty'))
         product=get_object_or_404(Product,id=product_id)
-        cart.add(product=product)
+        cart.add(product=product , quantity=product_qty)
         cart_quantity=cart.__len__()
         response=JsonResponse({'qty':cart_quantity})
         return response
@@ -18,7 +34,13 @@ def add_cart(request):
 
 
 def update_cart(request):
-    pass
+    cart=Cart(request)
+    if request.POST.get("action") == "post":
+        product_id=int(request.POST.get('product_id'))
+        product_qty=int(request.POST.get('product_qty'))
+        cart.update(product=product_id,quantity=product_qty)
+        response=JsonResponse({'qty':product_qty})
+        return response
 
 
 
